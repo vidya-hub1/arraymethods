35cf08d34e43e02f21df38198a8c28aa34bc0d12
shashank boddula <vidyaboddula625@gmail.com>
2024-12-23 09:19:29 +0530

    array

---
+++ js.js
@@ -0,0 +1,63 @@
+
+
+var a = ['1','2','3','4','5'],
+a2 = a.length;
+console.log(a2)
+
+a.push('6');
+console.log(a)
+
+a.pop()
+console.log(a)
+
+a.unshift('10');
+console.log(a)
+
+a.shift();
+console.log(a)
+
+a.reverse();
+console.log(a)
+
+a1 = a.indexOf('2');
+console.log(a1)
+
+console.log(a.includes('20'))
+
+console.log(a.sort())
+
+var str = "hello guys" 
+var split = str.split("");
+console.log(split)
+
+var a = ['rani','raju','seetha','geetha'];
+a.push('latha')
+a.shift()
+console.log(a)
+
+console.log(a.reverse())
+var split = a.join(' ');
+console.log(split)
+
+var a = ["kiwi","oranges","custardapple","banana","muskmelon"];
+a.sort();
+a.pop();
+console.log(a)
+ 
+var a = ['shinchan','doraemon'];
+a.unshift('nobita','jian');
+a.shift();
+a1 = a.length;
+console.log(a)
+console.log(a1)
+
+var a = ["kiwi","oranges","custardapple","banana","muskmelon"];
+var b = ['shinchan','doraemon'];
+var concat = a.concat(b);
+console.log(concat)
+var sor = concat.sort();
+console.log(sor)
+sor.pop();
+console.log(sor)
+
+/
